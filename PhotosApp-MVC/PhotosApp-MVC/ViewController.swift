9316670b35d1d98f6e07902ff682b02abf8437f7
System Administrator <root@Pavels-MacBook-Pro.local>
2019-05-02 23:50:34 +0300

    Added PhotosApp-MVC project

---
+++ PhotosApp-MVC/PhotosApp-MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosApp-MVC
+//
+//  Created by Pavel Palancica on 5/2/19.
+//  Copyright © 2019 Pavel Palancica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
